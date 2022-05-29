009a0b29feb388300d890bc763c3f67065aefb7a
Trevor Bos <trevor.bos@outlook.com>
2022-05-29 15:38:20 -0400

    Added questions, and linked the questions JS file to the HTML page.

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+
+var quizQuestions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts"
+    },
+    {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+      answer: "Parentheses"
+    },
+    {
+      question: "Which of these is not used to loop?",
+      choices: ["For", "While", "Foreach", "Sequence"],
+      answer: "Sequence"
+    },
+    {
+      question: "Which of these is not a way to save a variable",
+      choices: ["Vet", "Var", "Let", "Const"],
+      answer: "Vet"
+    },
+    {
+      question: "What is the main styling language used to make your website look good?",
+      choices: ["BOS", "JAS", "CS", "CSS"],
+      answer: "CSS"
+    },
+  ];
