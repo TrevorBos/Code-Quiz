6781c46ac14062fd8b01e553797fd66d1b9b7043
Trevor Bos <trevor.bos@outlook.com>
2022-05-29 15:32:26 -0400

    Started pseudo coding the JS file, figuring out potential functions and things needed. Created a second JS file to hold the questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,27 @@ var questionAnswers = document.querySelectorAll("#quizBox button");
 var highScoreArray = [];
 // Retrieve the highscore information if it exists, or keep it blank if it doesnt
 (localStorage.getItem ("highScoreArray")) ? highScoreArray = JSON.parse(localStorage.getItem("highScoreArray")): highScoreArray = [];
+
+//This is where the functions will go
+
+// Potential functions that will be needed / wanted
+
+// Function for calling elements without having to write them out all the time
+
+// Function for hiding all the quiz sections until chosen
+
+// Function for resetting the HTML to display the score information properly
+
+// Function for setting up the question information in the quizBox
+
+// Function for changing the question, and also tell the player whether they got the question right or wrong
+
+// Function for the timer events
+
+// Function to start timer and start giving the questions to the player
+
+// Function for handling what happens when a correct answer is given, or if its incorrect 
+
+// Function for error messages
+
+// High Score Control
